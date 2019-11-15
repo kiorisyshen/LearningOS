805c2d80c50361ca98c1d7b87c24a87a2f3c394a
kiorisyshen <kiorisyshen@gmail.com>
2019-11-15 19:31:19 +0900

    10-32bit-enter

--- main.asm
+++ main.asm
@@ -1,32 +1,28 @@
-[org 0x7c00]
-    mov bp, 0x8000 ; set the stack safely away from us
+[org 0x7c00] ; bootloader offset
+    mov bp, 0x9000 ; set the stack
     mov sp, bp
 
-    mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
-    mov dh, 2 ; read 2 sectors
-    ; the bios sets 'dl' for our boot disk number
-    ; if you have trouble, use the '-fda' flag: 'qemu -fda file.bin'
-    call disk_load
-
-    mov dx, [es:bx] ; retrieve the first loaded word, 0xdada
-    call print_hex
-
+    mov bx, MSG_REAL_MODE
+    call print ; This will be written after the BIOS messages
     call print_nl
 
-    mov dx, [es:bx + 512] ; first word from second loaded sector, 0xface
-    call print_hex
-
-    jmp $
+    call switch_to_pm
+    jmp $ ; this will actually never be executed
 
 %include "boot_sect_print.asm"
-%include "boot_sect_print_hex.asm"
-%include "boot_sect_disk.asm"
+%include "32bit-gdt.asm"
+%include "32bit-print.asm"
+%include "32bit-switch.asm"
 
-; Magic number
-times 510 - ($-$$) db 0
+[bits 32]
+BEGIN_PM: ; after the switch we will get here
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm ; Note that this will be written at the top left corner
+    jmp $
+
+MSG_REAL_MODE db "Started in 16-bit real mode", 0
+MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
+
+; bootsector
+times 510-($-$$) db 0
 dw 0xaa55
-
-; boot sector = sector 1 of cyl 0 of head 0 of hdd 0
-; from now on = sector 2 ...
-times 256 dw 0xdada ; sector 2 = 512 bytes
-times 256 dw 0xface ; sector 3 = 512 bytes
