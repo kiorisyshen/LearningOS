0b6a7fa360be1164d42ee4a516c3f05fe4e60dbe
kiorisyshen <kiorisyshen@gmail.com>
2019-11-15 17:00:21 +0900

    07-bootsector-disk

--- main.asm
+++ main.asm
@@ -1,27 +1,32 @@
-mov ah, 0x0e ; tty
+[org 0x7c00]
+    mov bp, 0x8000 ; set the stack safely away from us
+    mov sp, bp
 
-mov al, [the_secret]
-int 0x10 ; we already saw this doesn't work, right?
+    mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
+    mov dh, 2 ; read 2 sectors
+    ; the bios sets 'dl' for our boot disk number
+    ; if you have trouble, use the '-fda' flag: 'qemu -fda file.bin'
+    call disk_load
 
-mov bx, 0x7c0 ; remember, the segment is automatically <<4 for you
-mov ds, bx
-; WARNING: from now on all memory references will be offset by 'ds' implicitly
-mov al, [the_secret]
-int 0x10
+    mov dx, [es:bx] ; retrieve the first loaded word, 0xdada
+    call print_hex
 
-mov al, [es:the_secret]
-int 0x10 ; doesn't look right... isn't 'es' currently 0x000?
+    call print_nl
 
-mov bx, 0x7c0
-mov es, bx
-mov al, [es:the_secret]
-int 0x10
+    mov dx, [es:bx + 512] ; first word from second loaded sector, 0xface
+    call print_hex
 
+    jmp $
 
-jmp $
+%include "boot_sect_print.asm"
+%include "boot_sect_print_hex.asm"
+%include "boot_sect_disk.asm"
 
-the_secret:
-    db "X"
-
+; Magic number
 times 510 - ($-$$) db 0
 dw 0xaa55
+
+; boot sector = sector 1 of cyl 0 of head 0 of hdd 0
+; from now on = sector 2 ...
+times 256 dw 0xdada ; sector 2 = 512 bytes
+times 256 dw 0xface ; sector 3 = 512 bytes
