99b34a25f19de86bc8d19391186939000387e44b
kiorisyshen <kiorisyshen@gmail.com>
2019-11-18 23:59:20 +0900

    Change make tool to ninja

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir -p build
-cd build
+pushd build
 
 ###############
 # 1. Build boot loader
@@ -17,17 +17,18 @@ echo "[Done] Build boot loader"
 echo "[Start] Build kernel entry"
 nasm ../kernel_entry.asm -f elf -o kernel_entry.o
 echo "[Done] Build kernel entry"
+popd
 
 ###############
 # 3. Build C kernel
 ###############
 echo "[Start] Build kernel"
-cmake -GNinja ..
 ninja -v
-# cmake --build . #-- -j4
 echo "[Done] Build kernel"
 
 ###############
 # 4. Cat kernel to bootsect
 ###############
+pushd build
 cat bootsect.bin kernel.bin > os-image.bin
+popd
