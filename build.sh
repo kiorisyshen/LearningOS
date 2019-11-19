e9df5b4607e5d303a17e0a43c779938b88083473
kiorisyshen <kiorisyshen@gmail.com>
2019-11-19 15:33:47 +0900

    organize file into folders, build using ninja

--- build.sh
+++ build.sh
@@ -5,43 +5,13 @@ SHELL_FORE_RED='\e[31m'
 SHELL_FORE_GREEN='\e[32m'
 SHELL_NC='\e[39m' # Default
 
+printf "$SHELL_FORE_GREEN\nClean build folder$SHELL_NC\n"
+set -x
 mkdir -p build
 rm -rf build/*
-pushd build
-
-###############
-# 1. Build boot loader
-###############
-printf "$SHELL_FORE_GREEN\nBuilding boot loader$SHELL_NC\n"
-set -x
-nasm -i ../ -f bin ../bootsect.asm -o bootsect.bin
 set +x
 
-###############
-# 2. Build kernel entry
-###############
-printf "$SHELL_FORE_GREEN\nBuild kernel entry$SHELL_NC\n"
-set -x
-nasm ../kernel_entry.asm -f elf -o kernel_entry.o
-set +x
-
-popd
-
-###############
-# 3. Build C kernel
-###############
 printf "$SHELL_FORE_GREEN\nBuild kernel$SHELL_NC\n"
 ninja -v
-
-
-###############
-# 4. Cat kernel to bootsect
-###############
-printf "$SHELL_FORE_GREEN\nGenerat os-image.bin$SHELL_NC\n"
-pushd build
-set -x
-cat bootsect.bin kernel.bin > os-image.bin
-set +x
-popd
 
 printf "$SHELL_FORE_GREEN\nbuild done! Output files in ./build$SHELL_NC\n"
--- cross_compiler/build_cross_compiler.sh
+++ cross_compiler/build_cross_compiler.sh
@@ -27,51 +27,76 @@ export PATH="$PREFIX/bin:$PATH"
 BINUTIL=binutils-2.33.1
 GCCNAME=gcc-9.2.0
 
+GDBNAME=gdb-8.3
+
 ############################
 # Third, build binutils
 ############################
-mkdir -p $(pwd)/tmp/src
-pushd $(pwd)/tmp/src
+# mkdir -p $(pwd)/tmp/src
+# pushd $(pwd)/tmp/src
 
-if ! [ -f $BINUTIL.tar.gz ]; then
-    printf "$SHELL_FORE_GREEN\nDownloading $BINUTIL $SHELL_NC\n"
-    curl -O http://ftp.gnu.org/gnu/binutils/$BINUTIL.tar.gz # If the link 404's, look for a more recent version
-fi
+# if ! [ -f $BINUTIL.tar.gz ]; then
+#     printf "$SHELL_FORE_GREEN\nDownloading $BINUTIL $SHELL_NC\n"
+#     curl -O http://ftp.gnu.org/gnu/binutils/$BINUTIL.tar.gz # If the link 404's, look for a more recent version
+# fi
 
-printf "$SHELL_FORE_GREEN\nExtracting $BINUTIL $SHELL_NC\n"
-tar xf $BINUTIL.tar.gz
+# printf "$SHELL_FORE_GREEN\nExtracting $BINUTIL $SHELL_NC\n"
+# tar xf $BINUTIL.tar.gz
 
-mkdir -p binutils-build
-cd binutils-build
-printf "$SHELL_FORE_GREEN\nBuilding $BINUTIL $SHELL_NC\n"
-../$BINUTIL/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
-make all install 2>&1 | tee make.log
+# mkdir -p binutils-build
+# cd binutils-build
+# printf "$SHELL_FORE_GREEN\nBuilding $BINUTIL $SHELL_NC\n"
+# ../$BINUTIL/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
+# make all install 2>&1 | tee make.log
 
-popd
+# popd
 
 ############################
 # Final, build gcc
 ############################
+# mkdir -p $(pwd)/tmp/src
+# pushd $(pwd)/tmp/src
+
+# if ! [ -f $BINUTIL.tar.gz ]; then
+#     printf "$SHELL_FORE_GREEN\nDownloading $BINUTIL $SHELL_NC\n"
+#     curl -O https://ftp.gnu.org/gnu/gcc/$GCCNAME/$GCCNAME.tar.gz
+# fi
+
+# printf "$SHELL_FORE_GREEN\nExtracting $GCCNAME $SHELL_NC\n"
+# tar xf gcc-9.2.0.tar.gz
+# mkdir -p gcc-build
+# cd gcc-build
+# printf "$SHELL_FORE_GREEN\nConfiguring $GCCNAME $SHELL_NC\n"
+# ../$GCCNAME/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-languages=c++ --without-headers --with-gmp=/usr/local --with-mpfr=/usr/local --with-mpc=/usr/local
+
+# printf "$SHELL_FORE_GREEN\nBuilding $GCCNAME $SHELL_NC\n"
+# make all-gcc -j4
+# make all-target-libgcc -j4
+# make install-gcc
+# make install-target-libgcc
+
+# popd
+
+############################
+# Additionally, choose to build gdb
+############################
 mkdir -p $(pwd)/tmp/src
 pushd $(pwd)/tmp/src
 
-if ! [ -f $BINUTIL.tar.gz ]; then
-    printf "$SHELL_FORE_GREEN\nDownloading $BINUTIL $SHELL_NC\n"
-    curl -O https://ftp.gnu.org/gnu/gcc/$GCCNAME/$GCCNAME.tar.gz
+if ! [ -f $GDBNAME.tar.gz ]; then
+    printf "$SHELL_FORE_GREEN\nDownloading $GDBNAME $SHELL_NC\n"
+    curl -O http://ftp.gnu.org/gnu/gdb/$GDBNAME.tar.gz
 fi
 
-printf "$SHELL_FORE_GREEN\nExtracting $GCCNAME $SHELL_NC\n"
-tar xf gcc-9.2.0.tar.gz
-mkdir -p gcc-build
-cd gcc-build
-printf "$SHELL_FORE_GREEN\nConfiguring $GCCNAME $SHELL_NC\n"
-../$GCCNAME/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-languages=c++ --without-headers --with-gmp=/usr/local --with-mpfr=/usr/local --with-mpc=/usr/local
+printf "$SHELL_FORE_GREEN\nExtracting $GDBNAME $SHELL_NC\n"
+tar xf $GDBNAME.tar.gz
+mkdir -p gdb-build
+cd gdb-build
+../$GDBNAME/configure --target="$TARGET" --prefix="$PREFIX" --program-prefix=i386-elf-
 
-printf "$SHELL_FORE_GREEN\nBuilding $GCCNAME $SHELL_NC\n"
-make all-gcc -j4
-make all-target-libgcc -j4
-make install-gcc
-make install-target-libgcc
+printf "$SHELL_FORE_GREEN\nBuilding $GDBNAME $SHELL_NC\n"
+make -j4
+make install
 
 popd
 
