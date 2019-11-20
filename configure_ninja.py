72ba5a9f0bff5f5388f1a04ae8a38326e56d47fc
kiorisyshen <kiorisyshen@gmail.com>
2019-11-20 12:16:39 +0900

    Configure ninja using python script

--- configure_ninja.py
+++ configure_ninja.py
@@ -12,20 +12,74 @@ projRoot = os.getcwd()
 srcRoot = os.path.join(projRoot, "src")
 buildRoot = check_dir(os.path.join(projRoot, "build"))
 
-CC = os.path.join(projRoot, "cross_compiler",
-                  "i386elfgcc", "bin", "i386-elf-g++")
-CFlag = "-ffreestanding -g"
+ASM = "nasm"
+ASMFlag = ""
 
-linker = os.path.join(projRoot, "cross_compiler",
-                      "i386elfgcc", "bin", "i386-elf-ld")
-linkCross_flag = "-Ttext 0x1000"
+CC = os.path.join(projRoot, "cross_compiler", "i386elfgcc", "bin", "i386-elf-g++")
+CFlag = "-ffreestanding"
+
+linker = os.path.join(projRoot, "cross_compiler", "i386elfgcc", "bin", "i386-elf-ld")
+linkFflag = "-Ttext 0x1000"
+
 
 with open(os.path.join(buildRoot, "build.ninja"), "w") as buildfile:
     n = Writer(buildfile)
-    n.rule("compileASMbin", "nasm -i " + srcRoot + " -f bin $in -o $out")
 
-    # Build for bootsect
-    n.build(os.path.join(buildRoot, "bootsect.bin"),
-            "compileASMbin", inputs=[os.path.join(srcRoot, "boot", "bootsect.asm")])
+    def compileASM(out, inputs, asmflags):
+        n.build(os.path.join(buildRoot, out),
+                rule="compileASM",
+                inputs=inputs,
+                variables={"ASMFlag": ASMFlag + " " + asmflags})
 
-    n.default(os.path.join(buildRoot, "bootsect.bin"))
+    def compileCPP(out, inputs, cflags):
+        n.build(os.path.join(buildRoot, out),
+                rule="compileCPP",
+                inputs=inputs,
+                variables={"CFlag": CFlag + " " + cflags})
+
+    def link(out, inputs, linkflags):
+        n.build(os.path.join(buildRoot, out),
+                rule="link",
+                inputs=[os.path.join(buildRoot, obj) for obj in inputs],
+                variables={"linkFflag": linkFflag + " " + linkflags})
+
+    def packImage(out, inputs):
+        n.build(os.path.join(buildRoot, out),
+                rule="cat",
+                inputs=[os.path.join(buildRoot, obj) for obj in inputs])
+
+    n.rule("compileASM", ASM + " $in -o $out -i " + srcRoot + " $ASMFlag")
+    n.rule("compileCPP", CC + " -c $in -o $out -I " + srcRoot + " $CFlag")
+    n.rule("link", linker + " $in -o $out $linkFflag")
+    n.rule("cat", "cat $in > $out")
+
+    # Build use compileASM
+    compileASM(out="bootsect.bin",
+               inputs=[os.path.join(srcRoot, "boot", "bootsect.asm")],
+               asmflags="-f bin")
+
+    compileASM(out="kernel_entry.o",
+               inputs=[os.path.join(srcRoot, "boot", "kernel_entry.asm")],
+               asmflags="-f elf")
+
+    # Build use compileCPP
+    # TODO: scan folder?
+    # src/drivers
+    compileCPP(out="drivers.o",
+               inputs=[os.path.join(srcRoot, "drivers", "ports.cpp")],
+               cflags="-g")
+    # src/kernel
+    compileCPP(out="kernel.o",
+               inputs=[os.path.join(srcRoot, "kernel", "kernel.cpp")],
+               cflags="-g")
+    # link kernel & driver
+    link(out="kernel.bin",
+         inputs=["kernel_entry.o", "kernel.o", "drivers.o"],
+         linkflags="--oformat binary")
+
+    packImage(out="os-image.bin",
+              inputs=["bootsect.bin", "kernel.bin"])
+
+    n.default(os.path.join(buildRoot, "os-image.bin"))
+
+print("Configuring done!")
